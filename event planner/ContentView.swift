f18252549972d78d22d96227a6510ef82c72dec2
Aditya Patwal <adityapatwal07@gmail.com>
2024-07-22 13:01:31 -0400

    Initial Commit

---
+++ event planner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  event planner
+//
+//  Created by Aditya Patwal on 7/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ event planner/event_plannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  event_plannerApp.swift
+//  event planner
+//
+//  Created by Aditya Patwal on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct event_plannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
